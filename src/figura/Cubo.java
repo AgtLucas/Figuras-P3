b559fc9af656f26216b98c6ec0b8cceeda6bf94c
Lucas <metallucas.ds@gmail.com>
2013-03-21 23:05:31 -0300

    Adicionado objeto quadrado para a classe cubo

--- src/figura/Cubo.java
+++ src/figura/Cubo.java
@@ -9,7 +9,8 @@ package figura;
  * @author AgtLucas
  */
 class Cubo extends Figura3D {
-
+    Quadrado quadrado = new Quadrado();
+    
     @Override
     double CalcularVolume() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
